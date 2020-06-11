e6089bab0f2a5e3e960c5648b872215f0cac7021
Alonso Ninalaya <pikachualonso@gmail.com>
2020-06-11 19:56:26 -0400

    Updates

---
+++ precache-manifest.5d530f024a1d3028c387fb9940765829.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a419225585316f668c7168d1edbf4e3",
+    "url": "/corona-virus-update/index.html"
+  },
+  {
+    "revision": "77227b877601dfbd9382",
+    "url": "/corona-virus-update/static/css/main.8148fbc9.chunk.css"
+  },
+  {
+    "revision": "5b884dd877fad6b5a979",
+    "url": "/corona-virus-update/static/js/2.c3fb7dbf.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-virus-update/static/js/2.c3fb7dbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77227b877601dfbd9382",
+    "url": "/corona-virus-update/static/js/main.8d6c54b2.chunk.js"
+  },
+  {
+    "revision": "b1672662f1095d82541b",
+    "url": "/corona-virus-update/static/js/runtime-main.fd8d2195.js"
+  }
+]);
